8aaadf2e16a6460d1af7260a904e2bff3984cea7
Ashish Jadhav <ashishjadhav28999@gmail.com>
2023-07-29 17:01:41 +0530

    APIs and Backend commits

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo (id, username, description, target_date, done)
+values (10001, 'Ashish', 'Learn Advance Java', CURRENT_DATE(), false);
+
+insert into todo (id, username, description, target_date, done)
+values (10002, 'Ashish', 'Learn Full Stack Development', CURRENT_DATE(), false);
+
+insert into todo (id, username, description, target_date, done)
+values (10003, 'Ashish', 'Learn Docker, Kubernetes and Cloud', CURRENT_DATE(), false);
+
+insert into todo (id, username, description, target_date, done)
+values (10004, 'Ashish', 'Learn Competitive Prog and DSA', CURRENT_DATE(), false);
+
+insert into todo (id, username, description, target_date, done)
+values (10005, 'Ashish', 'Get Cloud Certified!', CURRENT_DATE(), false);
